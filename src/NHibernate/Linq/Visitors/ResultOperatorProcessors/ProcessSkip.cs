af86ac837e5f32a7eacd15d4ee8a56749e1c7337
Reynolm <m.reynolds@soslegal.co.uk>
2014-03-05 11:05:03 +0000

    Update ProcessSkip.cs

--- src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessSkip.cs
+++ src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessSkip.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+using System.Linq.Expressions;
 using NHibernate.Engine.Query;
 using NHibernate.Param;
 using Remotion.Linq.Clauses.ResultOperators;
@@ -14,14 +14,33 @@ namespace NHibernate.Linq.Visitors.ResultOperatorProcessors
 			VisitorParameters parameters = queryModelVisitor.VisitorParameters;
 			NamedParameter namedParameter;
 
+            /** https://nhibernate.jira.com/browse/NH-3021
+            * Temporary fix for Skip for dialects that support constant limiting only
+            * */
+            if (parameters.SessionFactory.Dialect.SupportsLimit && !parameters.SessionFactory.Dialect.SupportsVariableLimit)
+            {
+                tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+                return;
+            }
+
 			if (parameters.ConstantToParameterMap.TryGetValue(resultOperator.Count as ConstantExpression, out namedParameter))
 			{
 				parameters.RequiredHqlParameters.Add(new NamedParameterDescriptor(namedParameter.Name, null, false));
-				tree.AddSkipClause(tree.TreeBuilder.Parameter(namedParameter.Name));
-			}
-			else
-			{
-				tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+            //    tree.AddSkipClause(tree.TreeBuilder.Parameter(namedParameter.Name));
+            //}
+            //else
+            //{
+            //    tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+
+                 tree.AddSkipClause(tree.TreeBuilder.Parameter(namedParameter.Name));
+ 			}
+            else
+            {
+                tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+            //}
+
+                /* end of fix */
+
 			}
 		}
 
