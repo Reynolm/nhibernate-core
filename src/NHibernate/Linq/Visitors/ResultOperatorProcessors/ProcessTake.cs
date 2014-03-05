5e8545e81b3a317d12bfe65ca1bdf0e278ab1987
Reynolm <m.reynolds@soslegal.co.uk>
2014-03-05 11:03:34 +0000

    Update ProcessTake.cs

--- src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessTake.cs
+++ src/NHibernate/Linq/Visitors/ResultOperatorProcessors/ProcessTake.cs
@@ -1,27 +1,46 @@
-﻿using System.Linq.Expressions;
+using System.Linq.Expressions;
 using NHibernate.Engine.Query;
 using NHibernate.Param;
 using Remotion.Linq.Clauses.ResultOperators;
 
 namespace NHibernate.Linq.Visitors.ResultOperatorProcessors
 {
-	public class ProcessTake : IResultOperatorProcessor<TakeResultOperator>
+	public class ProcessSkip : IResultOperatorProcessor<SkipResultOperator>
 	{
-		#region IResultOperatorProcessor<TakeResultOperator> Members
+		#region IResultOperatorProcessor<SkipResultOperator> Members
 
-		public void Process(TakeResultOperator resultOperator, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
+		public void Process(SkipResultOperator resultOperator, QueryModelVisitor queryModelVisitor, IntermediateHqlTree tree)
 		{
 			VisitorParameters parameters = queryModelVisitor.VisitorParameters;
 			NamedParameter namedParameter;
 
+            /** https://nhibernate.jira.com/browse/NH-3021
+            * Temporary fix for Skip for dialects that support constant limiting only
+            * */
+            if (parameters.SessionFactory.Dialect.SupportsLimit && !parameters.SessionFactory.Dialect.SupportsVariableLimit)
+            {
+                tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+                return;
+            }
+
 			if (parameters.ConstantToParameterMap.TryGetValue(resultOperator.Count as ConstantExpression, out namedParameter))
 			{
 				parameters.RequiredHqlParameters.Add(new NamedParameterDescriptor(namedParameter.Name, null, false));
-				tree.AddTakeClause(tree.TreeBuilder.Parameter(namedParameter.Name));
-			}
-			else
-			{
-				tree.AddTakeClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+            //    tree.AddSkipClause(tree.TreeBuilder.Parameter(namedParameter.Name));
+            //}
+            //else
+            //{
+            //    tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+
+                 tree.AddSkipClause(tree.TreeBuilder.Parameter(namedParameter.Name));
+ 			}
+            else
+            {
+                tree.AddSkipClause(tree.TreeBuilder.Constant(resultOperator.GetConstantCount()));
+            //}
+
+                /* end of fix */
+
 			}
 		}
 
